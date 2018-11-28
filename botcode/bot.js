ce2806f9de1ef4b591641fd5354b5f0b96ca4edc
Valiant Hekert <00093215@student.rocleiden.nl>
2018-11-28 22:31:47 +0100

    maybe fucntion was out of scoop

--- botcode/bot.js
+++ botcode/bot.js
@@ -18,18 +18,19 @@ bot.on('ready', function (evt) {
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
 
-// costum functions
-function speak(msg){
-  bot.sendMessage({
-    to: channelID,
-    message: msg + ''
-  });
-}
-
 
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `$`
+    
+    // shorten message
+    function speak(msg){
+      bot.sendMessage({
+        to: channelID,
+        message: msg + ''
+      });
+    }
+    
     if (message.substring(0, 1) == '$') {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
