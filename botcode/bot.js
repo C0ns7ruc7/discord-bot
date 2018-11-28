ed6753b6e3ee6019ce13781fdb6f73f88370cdb7
valiant <vbshekert@hotmail.com>
2018-11-28 13:41:47 +0100

    added replies
    -add more

--- botcode/bot.js
+++ botcode/bot.js
@@ -19,21 +19,47 @@ bot.on('ready', function (evt) {
 });
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
+    // It will listen for messages that will start with `$`
+    if (message.substring(0, 1) == '$') {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
        
         args = args.splice(1);
         switch(cmd) {
-            // !ping
             case 'ping':
                 bot.sendMessage({
                     to: channelID,
                     message: 'Pong!'
                 });
             break;
-            // Just add any case commands if you want to..
+			
+			case 'say':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'no'
+                });
+            break;
+			
+			case 'scream':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
+                });
+            break;
+			
+			case 'lore':
+                bot.sendMessage({
+                    to: channelID,
+                    message: '_This world is filled with Suspendium, it floats and not too much is known about it except that it\'s not healthy to be around. It is also often associated with magic._'
+                });
+            break;
+			
+			default:
+				bot.sendMessage({
+						to: channelID,
+						message: 'ehm... sorry, I don\'t have anything to say about that, _yet_'
+					});
+			break;
          }
      }
 });
