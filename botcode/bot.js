f21a943e89372f1b7c1ed755f8e75405c662bb30
valiant <vbshekert@hotmail.com>
2018-12-03 20:52:27 +0100

    intermitten push

--- botcode/bot.js
+++ botcode/bot.js
@@ -43,18 +43,16 @@ bot.on('message', function (user, userID, channelID, message, evt) {
         var cmd = args[0].toUpperCase();
         args = args.splice(1);
         switch(cmd) {
+			case 'HELP': speak(
+'this is the help function of the molten bot, I have the following commands that you can use, ' + user + '! \n\n ok first up i\'m still being made so, be gentle... \n\n I have \`Ping\`, \`pong\`, \`bing\`, \`bong\`, \`ree\`, \`scream\` they are what they are, use em an I will come back at you >=\) \n then I have the \`say\` comand, it makes me repeat you... I don\'t know why that is here. \n I also keep track of \`lore\` things, you can add things with \`addlore\` and remove with \`rmvlore\` to get rid of it(wip). \n\n there are a bunch of other things in the works'
+			); 
+			break;
+			
             case 'PING': speak('Pong!'); break;
 			case 'PONG': speak('Ping!'); break;
 			case 'BING': speak('Bong!'); break;
 			case 'BONG': speak('Bing!'); break;
-            
-            case 'REE':
-                speak('ReeEeEeEEE!');
-            break;
-			
-			case 'UBERGRAD':
-                speak('_^ *`spingrad`_');
-            break;
+            case 'REE':  speak('ReeEeEeEEE!'); break;
             
             case 'SAY':
                 var cmd = args[0];
@@ -72,6 +70,26 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 						speak('are you daft?');
 					}else{
 						speak('nope, not saying that and you can\'t make me');
+					}
+                }
+            break;
+			
+			case 'SAYTO':
+                var cmd = args[0];
+                if (cmd && cmd.substring(0, 1) !== '$'){
+					var result = '';
+                	for (II in args){
+						result = result + ' ' + args[II];
+					}
+					speak(result);
+                }else{
+					var aNumber = Math.floor(Math.random() * 10);
+					if (aNumber >= 6){
+						speak('are you sure?');
+					}else if(aNumber <= 3){
+						speak('ok...?');
+					}else{
+						speak('no you');
 					}
                 }
             break;
@@ -97,7 +115,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 					}else{
 						var find = args.toString().toUpperCase();
 						speak(loreparsed[find]);
-						logger.info("finding: " + find);
+//						logger.info("finding: " + find);
 					}
                 }else if (lore == 1){
                   speak('... please state \`lore\` and a \`topic\`');
@@ -113,13 +131,27 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 					if (err) {
 						return console.log(err);
 					}
-					var result = data.replace('\"\}', '\", \"'+ args[0].toUpperCase().replace(/,|\*|_|\`/gi, '') + '\": \"' + args.join(' ') + '\"\}').replace(/(?:\r\n|\r|\n)/g, ' ');
+					var result = data.replace('\"\}', '\", \"'+ args[0].toUpperCase().replace(/,|\*|_|\`|:/gi, '') + '\": \"' + args.join(' ') + '\"\}').replace(/(?:\r\n|\r|\n)/g, ' ');
 
 					fs.writeFile('./data/lore.json', result, 'utf8', function (err) {
 						if (err) return console.log(err);
 					});
 				});
-				speak('I tried!');  
+				speak('I added... something related to ' + args[0]);  
+			break;
+			
+			case 'RMVLORE':
+				fs.readFile('./data/lore.json', 'utf8', function (err,data) {
+					if (err) {
+						return console.log(err);
+					}
+					var result = data.replace('\"\}', '\", \"'+ args[0].toUpperCase().replace(/,|\*|_|\`|:/gi, '') + '\": \"' + args.join(' ') + '\"\}').replace(/(?:\r\n|\r|\n)/g, ' ');
+
+					fs.writeFile('./data/lore.json', result, 'utf8', function (err) {
+						if (err) return console.log(err);
+					});
+				});
+				speak('I added... something related to ' + args[0]);  
 			break;
             
             default:
