432381005edd0aa42d9692606fe9ef330eaea12c
valiant <vbshekert@hotmail.com>
2018-12-12 23:33:02 +0100

    le-update

--- botcode/bot.js
+++ botcode/bot.js
@@ -37,22 +37,38 @@ bot.on('message', function (user, userID, channelID, message, evt) {
         message: msg + ''
       });
     }
+	function rNum(num){
+		return Math.floor(Math.random() * num)
+	}
     
     if (message.substring(0, 1) == '$' || message.substring(0, 22) == '<@!517269535024349195>') {
         var args = message.substring(1).split(' ');
         var cmd = args[0].toUpperCase();
-        args = args.splice(1);
+        var args = args.splice(1);
+		var responce = ((rNum(10)) >= 5) ? '?' : '!';
+		
         switch(cmd) {
 			case 'HELP': speak(
-'this is the help function of the molten bot, I have the following commands that you can use, ' + user + '! \n\n ok first up i\'m still being made so, be gentle... \n\n I have \`Ping\`, \`pong\`, \`bing\`, \`bong\`, \`ree\`, \`scream\` they are what they are, use em an I will come back at you >=\) \n then I have the \`say\` {msg} comand, it makes me repeat you... I don\'t know why that is here. \n I also keep track of \`lore\` [lore name] things, you can add things with \`addlore\` [frist word is key] {the rest} and remove with \`rmvlore\` to get rid of it(wip). \n there is also the \`mail\` [#channel] {msg}  where you can send to a channel \n\n there are a bunch of other things in the works \n don\'t forget I use \`$\`\'s to react'
+'this is the help function of the molten bot, I have the following commands that you can use, <@' + userID + '>! \n\n' + 
+'Ok first up i\'m still being made so, be gentle... \n\n' + 
+'- I have \`Ping\`, \`pong\`, \`bing\`, \`bong\`, \`ree\`, \`scream\`, \`beep\` and \`boop\` they are what they are, use em an I will come back at you >=\) \n' + 
+'- then I have the \`say\` {msg} comand, it makes me repeat you... I don\'t know why that is here. \n' + 
+'- I also keep track of \`lore\` [lore name] things \n' +
+'- you can add things with \`addlore\` [frist word is key] {the rest} \n' + 
+'- remove lore with \`rmvlore\` to get rid of it(wip). \n' + 
+'- there is also the \`mail\` [#channel] {msg}  where you can send to a channel \n' + 
+'- I also have the \`roll\` command, it works as a automatic 2d6 if you don\'t add comments \n\n' + 
+'there are a bunch of other things in the works \n' + 
+'don\'t forget I use \`$\`\'s to react'
 			); 
 			break;
 			
-            case 'PING': speak('Pong!'); break;
-			case 'PONG': speak('Ping!'); break;
-			case 'BING': speak('Bong!'); break;
-			case 'BONG': speak('Bing!'); break;
-            case 'REE':  speak('ReeEeEeEEE!'); break;
+            case 'PING': speak('**\`Pong' + responce + '\`**'); break;
+			case 'PONG': speak('**\`Ping' + responce + '\`**'); break;
+			case 'BING': speak('**\`Bong' + responce + '\`**'); break;
+			case 'BONG': speak('**\`Bing' + responce + '\`**'); break;
+			case 'BEEP': speak('**\`Boop' + responce + '\`**'); break;
+			case 'BOOP': speak('**\`Beep' + responce + '\`**'); break;
             
             case 'SAY':
                 var cmd = args[0];
@@ -61,9 +77,9 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                 	for (II in args){
 						result = result + ' ' + args[II];
 					}
-					speak(user + ' said: ' + result);
+					speak('_<@' + userID+ '> said:_ **' + result + '**');
                 }else{
-					var aNumber = Math.floor(Math.random() * 10);
+					var aNumber = rNum(10);
 					if (aNumber >= 6){
 						speak('you what?');
 					}else if(aNumber <= 3){
@@ -109,13 +125,24 @@ bot.on('message', function (user, userID, channelID, message, evt) {
             break;
             
             case 'SCREAM':
-            	var aNumber = Math.floor(Math.random() * 10);
+            	var aNumber = rNum(10);
 				if (aNumber >= 6){
-                    speak('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA!');
+                    speak(((rNum(10)) >= 5) ? 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA!' : '$ree');
                 }else if(aNumber <= 3){
-                    speak('AaAaAaAaAaAaAaAaAAaAaAaAaAaAaAaAaAAaAaAa!');
+                    speak(((rNum(10)) >= 5) ? 'AaAaAaAaAaAaAaAaAAaAaAaAaAaAaAaAaAAaAaAa!' : '***AAAAH!***' );
                 }else{
-                    speak('_\*Incoherent screaming*_');
+                    speak(((rNum(10)) >= 5) ? '_\*Incoherent screaming*_' : '\"...\" _the sounds were so high only a dog can hear them_');
+                }
+            break;
+			
+			case 'REE':
+            	var aNumber = rNum(10);
+				if (aNumber >= 6){
+                    speak(((rNum(10)) >= 5) ? 'ReeEeEeEEE!' : 'rEeeeEEEE!');
+                }else if(aNumber <= 3){
+                    speak(((rNum(10)) >= 5) ? '$scream' : 'rrrreeeeeeeEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE!');
+                }else{
+                    speak(((rNum(10)) >= 5) ? '_\*explodes with \`ree\`\'s*_' : '_\*Screaming frog sounds*_');
                 }
             break;
             
@@ -123,21 +150,18 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                 var cmd = args[0];
 				let loredata = fs.readFileSync('./data/lore.json');  
 				let loreparsed = JSON.parse(loredata); 
+				var result = '';
                 if(cmd) {
-					if(cmd == 'random'){
-						
-					}else{
-						var find = args.toString().toUpperCase();
-						speak(loreparsed[find]);
-//						logger.info("finding: " + find);
+					var find = args.toString().toUpperCase();
+					speak(loreparsed[find]);
+//					logger.info("finding: " + find);
+				}else{
+					var tempkeys = Object.keys(loreparsed)
+					for (var II in tempkeys){
+						result = result + tempkeys + '\n';
 					}
-                }else if (lore == 1){
-                  speak('... please state \`lore\` and a \`topic\`');
-                  const lore = 0;
-                }else{
-                  speak('That is a interesting topic, but there is simply too much of it to just speak about it in one sitting, how about we start with one subject first?');
-                  const lore = 1;
-                }
+					speak('I have the following topic(s) available:\n' + result);
+				}
             break;
 			
 			case 'ADDLORE':
@@ -176,6 +200,17 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 					speak('You can only do that in the \`bot commands\` channel');
 				}
 			break;
+			
+			case 'ROLL':
+				var cmd = args[0];
+				if(cmd){
+					speak('conditional roll');
+				}else{
+					var num1 = rNum(6) + 1;
+					var num2 = rNum(6) + 1;
+					speak( '<@' + userID+ '> has rolled **' + (num1 + num2) + '** ( \`' + num1 + '\`, \`' + num2 + '\` )');
+				}
+			break;
             
             default:
                 speak('ehm... ok? try running \`$help\`');
@@ -189,7 +224,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 		}
 	}); 
 
-	if (Math.floor(Math.random() * 30000) <= 200){
+	if (Math.floor(Math.random() * 30000) <= 10){
 		bot.sendMessage({
 			to: '481133060348182550',
 			message: '<@&481250276825890817> you should RP! join a side or do your own thing'
