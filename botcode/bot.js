a74f9d956721ae3a5b51942e6c61c38e3fdf23eb
valiant <vbshekert@hotmail.com>
2018-12-14 21:06:28 +0100

    BLYAT

--- botcode/bot.js
+++ botcode/bot.js
@@ -24,6 +24,7 @@ bot.on('ready', function (evt) {
     logger.info('Connected');
     logger.info('Logged in as: ');
     logger.info(bot.username + ' - (' + bot.id + ')');
+	bot.setPresence( 'game' );
 });
 
 bot.on('message', function (user, userID, channelID, message, evt) {
@@ -53,10 +54,11 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 'Ok first up i\'m still being made so, be gentle... \n\n' + 
 '- I have \`Ping\`, \`pong\`, \`bing\`, \`bong\`, \`ree\`, \`scream\`, \`beep\` and \`boop\` they are what they are, use em an I will come back at you >=\) \n' + 
 '- then I have the \`say\` {msg} comand, it makes me repeat you... I don\'t know why that is here. \n' + 
-'- I also keep track of \`lore\` [lore name] things \n' +
-'- you can add things with \`addlore\` [frist word is key] {the rest} \n' + 
+'- I also keep track of \`lore\` [lore name] things, do a empty command for a list. \n' +
+'- you can add things with \`addlore\` {[first word is key] the rest, explaining stuf} \n' + 
 '- remove lore with \`rmvlore\` to get rid of it(wip). \n' + 
-'- there is also the \`mail\` [#channel] {msg}  where you can send to a channel \n' + 
+'- there is also the \`mail\` [#channel(or when in DM a number)] {msg}  where you can send to a channel \n' + 
+'- a functional command is the \`whatis\` [@user/role OR #channel], it shows you the number needed for DM\'ing using the mail function \n' + 
 '- I also have the \`roll\` command, it works as a automatic 2d6 if you don\'t add comments \n\n' + 
 'there are a bunch of other things in the works \n' + 
 'don\'t forget I use \`$\`\'s to react'
@@ -90,6 +92,14 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                 }
             break;
 			
+			case 'WHATIS':
+                if (args[0]){
+					speak('_<@' + userID+ '> the deciphered things you said:_' + args.join(' ').replace(/<|>|@/gi, '').replace(/#/gi, ' Channel:').replace(/&/gi, ' Role:').replace(/!/gi, ' User:'));
+                }else{
+					speak('you forgot everything');
+                }
+            break;
+			
 			case 'ROOTSAYTO':
 				var sendto = args[0].replace(/<|#|>/g, '')
 				args = args.splice(1);
@@ -115,7 +125,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 					}
 					bot.sendMessage({
 						to: sendto + '',
-						message: '_A radio signal on frequency_ **\`[' + Math.floor((userID - channelID) / Date.now()) + ']\`** _is being received._ ' + result + ''
+						message: '**\`[' + Math.floor((channelID - userID) / Date.now()) + ']\`**:' + result + ''
 					});
 					speak('your message has been sent');
 				}else{
@@ -147,35 +157,35 @@ bot.on('message', function (user, userID, channelID, message, evt) {
             break;
             
             case 'LORE':
-                var cmd = args[0];
 				let loredata = fs.readFileSync('./data/lore.json');  
 				let loreparsed = JSON.parse(loredata); 
 				var result = '';
-                if(cmd) {
+                if(args[0]) {
 					var find = args.toString().toUpperCase();
 					speak(loreparsed[find]);
 //					logger.info("finding: " + find);
 				}else{
-					var tempkeys = Object.keys(loreparsed)
-					for (var II in tempkeys){
-						result = result + tempkeys + '\n';
-					}
-					speak('I have the following topic(s) available:\n' + result);
+					var result = ((Object.keys(loreparsed)).join('\n')).toLowerCase();
+					speak('_<@' + userID+ '>, I have the following topic(s) available:\n_ **' + result + '**');
 				}
             break;
 			
 			case 'ADDLORE':
-				fs.readFile('./data/lore.json', 'utf8', function (err,data) {
-					if (err) {
-						return console.log(err);
-					}
-					var result = data.replace('\"\}', '\", \"'+ args[0].toUpperCase().replace(/,|\*|_|\`|:/gi, '') + '\": \"' + args.join(' ') + '\"\}').replace(/(?:\r\n|\r|\n)/g, ' ');
+				if(args[0]){
+					fs.readFile('./data/lore.json', 'utf8', function (err,data) {
+						if (err) {
+							return console.log(err);
+						}
+						var result = data.replace('\"\}', '\", \"'+ args[0].toUpperCase().replace(/,|\*|_|\`|:/gi, '') + '\": \"' + args.join(' ').replace(/\"/g, '\\"') + '\"\}').replace(/(?:\r\n|\r|\n)/g, ' ');
 
-					fs.writeFile('./data/lore.json', result, 'utf8', function (err) {
-						if (err) return console.log(err);
+						fs.writeFile('./data/lore.json', result, 'utf8', function (err) {
+							if (err) return console.log(err);
+						});
 					});
-				});
-				speak('I added... something related to ' + args[0]);  
+					speak('I added... something related to ' + args[0]);  
+				}else{
+					speak('\`you eh... need to add some text, just start writing things after the \"$addlore\", you can add embellishments all you like too, by the way.\`');  
+				}
 			break;
 			
 			case '-RMVLORE':
@@ -202,8 +212,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 			break;
 			
 			case 'ROLL':
-				var cmd = args[0];
-				if(cmd){
+				if(args[0]){
 					speak('conditional roll');
 				}else{
 					var num1 = rNum(6) + 1;
