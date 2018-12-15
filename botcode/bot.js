7e30ae6ddf8231e7b591a85f8b642c0f885c5294
valiant <vbshekert@hotmail.com>
2018-12-15 21:34:13 +0100

    messing with the random funtion

--- botcode/bot.js
+++ botcode/bot.js
@@ -39,7 +39,16 @@ bot.on('message', function (user, userID, channelID, message, evt) {
       });
     }
 	function rNum(num){
-		return Math.floor(Math.random() * num)
+//		return Math.floor(Math.random() * num)
+//		return (Math.round(((Math.random() * Math.random()) *10) * Math.random() * 1000)) % num
+//		return Math.random()
+		
+		var data = ((Math.random()*10)+'').replace('.', '').split('');
+		var numb = data[data[0]] * 1;
+		if (numb >= num){
+			numb = numb % num;
+		}
+		return numb;
 	}
     
     if (message.substring(0, 1) == '$' || message.substring(0, 22) == '<@!517269535024349195>') {
