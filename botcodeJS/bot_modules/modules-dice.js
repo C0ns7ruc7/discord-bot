b99704d94248b00dca9629249e3bccad1280f5c8
valiant <00093215@student.rocleiden.nl>
2019-11-06 17:13:44 +0100

    Update modules-dice.js

--- botcodeJS/bot_modules/modules-dice.js
+++ botcodeJS/bot_modules/modules-dice.js
@@ -1,5 +1,18 @@
 module.exports = {	
 	dice: function(args, userID, fs, logger, evt){
-		
+		// require the dice-roller library
+		const { DiceRoller } = require('rpg-dice-roller');
+
+		// create a new instance of the DiceRoller
+		const diceRoller = new DiceRoller();
+
+		// roll the dice
+		diceRoller.roll('4d20-L');
+
+		// get the latest dice rolls from the log
+		let latestRoll = diceRoller.log.shift();
+
+		// output the latest roll - it has a toString method for nice output
+		return(latestRoll + '');
 	}	
 }
